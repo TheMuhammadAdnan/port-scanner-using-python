cc6ffc8d4a14d3781b0c23045b6700abf3da96c6
Adnan <adikhanbloch@gmail.com>
2022-01-29 01:54:42 +0500

    Remove extra spaces

--- port_scanner.py
+++ port_scanner.py
@@ -1,35 +1,22 @@
 """ 
-
 Simple Port scanner
-
 Credit: Adnan
-
 Ref: https://pk.linkedin.com/in/adnan-django-python
-
 """
-
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 target = input(
-
     """socket.gethostbyname expects a host name and not an URL.
-
 You must give www.osjajinci.com instead of http://www.osjajinci.com/
-
 Enter host or IP Address:"""
+)
 
-        )
 
 def pscan(port):
+    return s.connect_ex((target, port))
 
-	return s.connect_ex((target,port))
-
-for x in range(2000):
-
-	print("Scanning Port: ", x)
-
-	if pscan(x) == 0:
-
-		print("Port", x," is open!")
+for x in range(75, 85):
+    print("Scanning Port: ", x)
+    if pscan(x) == 0:
+        print("Port", x, " is open!")
